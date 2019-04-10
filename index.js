18504954e03a2e1f0c8a3b82c1637712c86db68d
Kimberly Heidorn <heidorkn@gmail.com>
2019-04-10 15:33:10 -0700

    very basic function lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+
+let heyJude = (verse) => {
+  return verse.repeat(16)
+}
+
+let prohibitedLanguage = (string) => {
+  let bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords);
+}
