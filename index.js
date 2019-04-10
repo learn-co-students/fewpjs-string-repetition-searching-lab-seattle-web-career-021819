952ffc553e3a1eb4a4f873c05a229f2ef738b083
ytsuji27 <ytsuji91@gmail.com>
2019-04-10 13:13:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+function heyJude() {
+  let str = 'Na na na na na na na, na na na na, hey Jude.';
+  return str.repeat(16);
+}
+
+let prohibitedLanguage = (str) => {
+  let reg = /candycorn|brusselssprouts/;
+  return str.search(reg);
+}
