eea2c0832888ce3b01893e069bf2faeb4df34796
github username <github email address>
2019-04-11 16:40:47 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+const heyJude = () => {
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+const prohibitedLanguage = (string) => {
+  const prohibited = /candycorn|brusselssprouts/
+  return string.search(prohibited)
+}
